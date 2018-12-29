445d77cc0d2fe191a651a321913993cdee042d62
rakin <rakin@rakins-MacBook-Pro.local>
2018-12-29 11:24:15 +0600

    init redis interface

---
+++ redis/interface.go
@@ -0,0 +1,17 @@
+package redis
+
+import "time"
+
+// Locker handles locking
+type Locker interface {
+	Lock(key string) error
+	Unlock(key string) error
+}
+
+// Set manages set operations
+type Set interface {
+	First() (kye string, err error)
+	Add(tim time.Time, key string) (err error)
+	UpdateTim(tim time.Time, key string) (err error)
+	Remove(key string) error
+}

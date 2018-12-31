6a7d381aadc50b3d42b0ec4875548571f80e4b08
rakin <rakin@rakins-MacBook-Pro.local>
2018-12-31 13:03:21 +0600

    add default life time of running task

--- cnst/cnst.go
+++ cnst/cnst.go
@@ -1,6 +1,11 @@
 package cnst
 
+import "time"
+
 const (
 	// App holds app name
 	App string = "redisjq"
+
+	// DefaultTaskLifeTime represents task's default life time
+	DefaultTaskLifeTime time.Duration = time.Second * 60
 )

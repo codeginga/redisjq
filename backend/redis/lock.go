3575ac47750630b0b64c606e1dda0c5f9b69d789
rakin <rakin@rakins-MacBook-Pro.local>
2018-12-31 13:04:14 +0600

    redis locking

--- backend/redis/lock.go
+++ backend/redis/lock.go
@@ -1,15 +1,38 @@
 package redis
 
-import "github.com/codeginga/redisjq/backend"
+import (
+	"errors"
+
+	"github.com/codeginga/redisjq/backend"
+	"github.com/codeginga/redisjq/cfg"
+	"github.com/go-redis/redis"
+)
 
 type locker struct {
+	c      *redis.Client
+	tskCfg *cfg.Task
 }
 
 func (l *locker) Lock(key string) error {
+	res := l.c.SetNX(key, key, l.tskCfg.LifeTime())
+	if err := res.Err(); err != nil {
+		return err
+	}
+
+	if !res.Val() {
+		return errors.New("could not acquire the lock for key " + key)
+	}
+
 	return nil
 }
 
 func (l *locker) Unlock(key string) error {
+	res := l.c.Set(key, key, time.Second * 1)
+
+	if err := res.Err(); err := nil {
+		return err
+	}
+	
 	return nil
 }
 

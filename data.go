17fae4cf138b3abaaf255b6b58bc652ab44f3a13
rakin <rakin@rakins-MacBook-Pro.local>
2019-01-01 13:13:34 +0600

    update message struct

--- data.go
+++ data.go
@@ -1,23 +1,46 @@
 package redisjq
 
+import (
+	"encoding/json"
+	"time"
+)
+
 // Message holds message for job queue
 type Message struct {
-	ID    string
-	Name  string
-	Retry int
-	Value string
+	// set qniqueue id of each task
+	ID string `json:"id"`
 
-	delay int
+	// Name defines task name
+	// set uniqueue name for different kind of  task
+	Name string `json:"name"`
+
+	Retry int    `json:"retry"`
+	Value string `json:"value"`
+
+	// task pop delay time in seconds
+	Delay int `json:"delay"`
 }
 
-// Delay retruns message pop delay
-func (m *Message) Delay() int {
-	return m.delay
+func (m *Message) popupTime() time.Time {
+	t := time.Now().Add(time.Second * time.Duration(m.Delay))
+	return t
 }
 
-// SetDelay sets message pop delay time
-func (m *Message) SetDelay(sec int) {
-	m.delay = sec
+// String converts Message to string
+func (m *Message) String() (val string, err error) {
+	bts, err := json.Marshal(m)
+	if err != nil {
+		return
+	}
+
+	val = string(bts)
+	return
+}
+
+// FrmString converts string to Message
+func (m *Message) FrmString(str string) (err error) {
+	err = json.Unmarshal([]byte(str), m)
+	return
 }
 
 // Worker defines worker

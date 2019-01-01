09ad405aacd39056d2f231b2811c66e13e5dc4bd
rakin <rakin@rakins-MacBook-Pro.local>
2019-01-01 13:38:03 +0600

    redis client

--- cfg/redis.go
+++ cfg/redis.go
@@ -1,8 +1,21 @@
 package cfg
 
+import "github.com/go-redis/redis"
+
 // Redis holds config for redis connection
 type Redis struct {
 	Addr     string
 	Password string
 	DB       int
+}
+
+func (r *Redis) client(c *redis.Client, err error) {
+	c = redis.NewClient(&redis.Options{
+		Addr:     r.Addr,
+		Password: r.Password,
+		DB:       r.DB,
+	})
+
+	_, err = c.Ping().Result()
+	return
 }

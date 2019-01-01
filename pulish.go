4063fc95896297cc84509c78c11eb2e4718a9263
rakin <rakin@rakins-MacBook-Pro.local>
2019-01-01 13:13:45 +0600

    implement publish

--- pulish.go
+++ pulish.go
@@ -1,10 +1,32 @@
 package redisjq
 
+import (
+	"github.com/codeginga/redisjq/backend"
+	"github.com/codeginga/redisjq/cnst"
+)
+
 type publisher struct {
+	task  backend.Task
+	set   backend.Set
+	qname string
 }
 
-func (p *publisher) Publish(msg Message) error {
-	return nil
+func (p *publisher) appQName() string {
+	return cnst.App + "_" + p.qname
+}
+
+func (p *publisher) Publish(msg Message) (err error) {
+	strMsg, err := msg.String()
+	if err != nil {
+		return
+	}
+
+	if err = p.task.Save(cnst.App+"_"+msg.ID, strMsg); err != nil {
+		return
+	}
+
+	err = p.set.Add(msg.popupTime(), msg.ID)
+	return
 }
 
 // NewPublisher returns new instance of Publisher

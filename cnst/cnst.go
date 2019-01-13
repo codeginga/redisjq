aa572a1acc7364c92e00d489c388f5021b76c679
rakin <rakin@rakins-MacBook-Pro.local>
2019-01-13 12:57:14 +0600

    update sleeping time

--- cnst/cnst.go
+++ cnst/cnst.go
@@ -9,6 +9,9 @@ const (
 	// Separator sperates/concates string
 	Separator string = "#@#"
 
+	// Lock is suffix of lock key
+	Lock string = "_lock"
+
 	// DefaultTaskRunTime represents default life time of running task
 	DefaultTaskRunTime time.Duration = time.Second * 60
 
@@ -16,7 +19,7 @@ const (
 	DefaultTaskMessageTTL time.Duration = time.Second * (60 * 10)
 
 	// SleepDuration defines sleep time after run all worker
-	SleepDuration time.Duration = time.Millisecond * 100
+	SleepDuration time.Duration = time.Millisecond * 400
 
 	// RedisEmptyMessage represents message for empty key
 	// "github.com/go-redis/redis" returns this value for empty key
